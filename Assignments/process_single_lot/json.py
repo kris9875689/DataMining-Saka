a71dde38e405d559fe988d57e90fb1a758a3b856
Krishna Saka <krishna09eee@gmail.com>
2017-10-09 22:29:54 -0500

    Add files via upload

---
+++ Assignments/process_single_lot/json.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 20:41:00 2017
+
+@author: keerthi
+"""
+
+from xmljson import parker, Parker
+from xmljson import yahoo
+from xml.etree.ElementTree import fromstring
+from json import dumps,loads
+import pprint as pp
+
+
+
+def xmltojson(file_name,out_file=None):
+    fp = open(file_name,'r')
+
+    xmldata = fp.read()
+
+    jsond = dumps(yahoo.data(fromstring(xmldata)))
+
+    jsond = loads(jsond)
+
+    spaces = jsond['parking']['space']
+
+    if not out_file is None:
+        f = open(out_file,'w')
+        f.write(dumps(spaces,indent=4, separators=(',', ': ')))
+        f.close()
+
+    for space in spaces:
+        print(space['contour'])
+        for point in space['contour']['point']:
+            print(point)
+
+if __name__=='__main__':
+    filename = 'C:\\Users\\keerthi\\.spyder-py3\\2012-12-07_17_12_25.xml'
+    xmltojson(filename,'project1.json')
